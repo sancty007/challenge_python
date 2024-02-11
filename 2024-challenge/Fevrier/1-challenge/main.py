ede8f5b66b71cbe97f6c01bc85d2f55504084d81
sancty <sanctifieryawmensah55@gmail.com>
2024-02-12 00:37:43 +0100

    solution-1

--- 2024-challenge/Fevrier/1-challenge/main.py
+++ 2024-challenge/Fevrier/1-challenge/main.py
@@ -1,4 +1,32 @@
-def separateur():
-    pass
+
+
+resultat = "bonjour-tout-le-monde"
+
+
+# Initialiser l'indice de départ pour le découpage
+
+
+def separateur(variable , char):
+ 
+    tableau = []  # Initialiser la liste pour stocker les parties séparées
+    indice_depart = 0
+
+    for i in range(len(variable)):
+        phrase = ''
+        if variable[i] == char:
+            phrase = variable[indice_depart:i] # interation jusqu'a la rencontre d'un tiret 
+
+            indice_depart = i + 1
+
+            tableau.append(phrase)
+
+    tableau.append(resultat[indice_depart:])
+
+    return tableau
+
+
+
 
 resultat = separateur("bonjour-tout-le-monde", "-")
+
+print(resultat)
