d25718a7b1cb064a65013dc0abec3562bd6b786f
sancty <sanctifieryawmensah55@gmail.com>
2024-02-15 08:26:11 +0100

    2-challenge

---
+++ 2024-challenge/Fevrier/2-challenge/main.py
@@ -0,0 +1,26 @@
+class ListePersonnalisee:
+    def __init__(self):
+        self.elements = []
+
+    def ajouter_element(self, element):
+        # Ajoute un élément à la liste personnalisée
+        pass
+
+    def supprimer_element(self, index):
+        # Supprime l'élément à l'index spécifié de la liste personnalisée
+        pass
+
+    def acces_element(self, index):
+        # Retourne l'élément à l'index spécifié de la liste personnalisée
+        pass
+
+# Exemple d'utilisation
+liste = ListePersonnalisee()
+liste.ajouter_element("Bonjour")
+liste.ajouter_element("tout")
+liste.ajouter_element("le")
+liste.ajouter_element("monde")
+
+print(liste.acces_element(0))  # Devrait afficher "Bonjour"
+liste.supprimer_element(1)
+print(liste.acces_element(1))  # Devrait afficher "le"
